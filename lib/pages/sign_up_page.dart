3b2b02e0bdc90cf60d7acf0e2876eb077b69833c
telanlah <33682009+telanlah@users.noreply.github.com>
2022-10-31 22:53:40 +0700

    firsts commit

---
+++ lib/pages/sign_up_page.dart
@@ -0,0 +1,274 @@
+import 'package:flutter/material.dart';
+import 'package:shamo/theme.dart';
+
+class SignUpPage extends StatelessWidget {
+  const SignUpPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Widget header() {
+      return Container(
+        margin: EdgeInsets.only(top: 30),
+        child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+          Text(
+            'Sign Up',
+            style: primaryTextStyle.copyWith(
+              fontSize: 24,
+              fontWeight: semiBold,
+            ),
+          ),
+          SizedBox(
+            height: 2,
+          ),
+          Text(
+            'Register and Happy Shopping',
+            style: subTitleTextStyle,
+          ),
+        ]),
+      );
+    }
+
+    Widget fullNameInput() {
+      return Container(
+        margin: EdgeInsets.only(top: 50),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Full Name',
+              style:
+                  primaryTextStyle.copyWith(fontSize: 16, fontWeight: medium),
+            ),
+            SizedBox(
+              height: 12,
+            ),
+            Container(
+              height: 50,
+              padding: EdgeInsets.symmetric(horizontal: 16),
+              decoration: BoxDecoration(
+                  color: backgroundColor2,
+                  borderRadius: BorderRadius.circular(12)),
+              child: Center(
+                  child: Row(
+                children: [
+                  Image.asset(
+                    'assets/icon_name.png',
+                    width: 17,
+                  ),
+                  SizedBox(
+                    width: 16,
+                  ),
+                  Expanded(
+                      child: TextFormField(
+                          style: primaryTextStyle,
+                          decoration: InputDecoration.collapsed(
+                            hintText: 'Your Full Name',
+                            hintStyle: subTitleTextStyle,
+                          ))),
+                ],
+              )),
+            )
+          ],
+        ),
+      );
+    }
+
+    Widget usernameInput() {
+      return Container(
+        margin: EdgeInsets.only(top: 20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Username',
+              style:
+                  primaryTextStyle.copyWith(fontSize: 16, fontWeight: medium),
+            ),
+            SizedBox(
+              height: 12,
+            ),
+            Container(
+              height: 50,
+              padding: EdgeInsets.symmetric(horizontal: 16),
+              decoration: BoxDecoration(
+                  color: backgroundColor2,
+                  borderRadius: BorderRadius.circular(12)),
+              child: Center(
+                  child: Row(
+                children: [
+                  Image.asset(
+                    'assets/icon_username.png',
+                    width: 17,
+                  ),
+                  SizedBox(
+                    width: 16,
+                  ),
+                  Expanded(
+                      child: TextFormField(
+                          style: primaryTextStyle,
+                          decoration: InputDecoration.collapsed(
+                            hintText: 'Your Username',
+                            hintStyle: subTitleTextStyle,
+                          ))),
+                ],
+              )),
+            )
+          ],
+        ),
+      );
+    }
+
+    Widget emailInput() {
+      return Container(
+        margin: EdgeInsets.only(top: 20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Email Address',
+              style:
+                  primaryTextStyle.copyWith(fontSize: 16, fontWeight: medium),
+            ),
+            SizedBox(
+              height: 12,
+            ),
+            Container(
+              height: 50,
+              padding: EdgeInsets.symmetric(horizontal: 16),
+              decoration: BoxDecoration(
+                  color: backgroundColor2,
+                  borderRadius: BorderRadius.circular(12)),
+              child: Center(
+                  child: Row(
+                children: [
+                  Image.asset(
+                    'assets/icon_email.png',
+                    width: 17,
+                  ),
+                  SizedBox(
+                    width: 16,
+                  ),
+                  Expanded(
+                      child: TextFormField(
+                          style: primaryTextStyle,
+                          decoration: InputDecoration.collapsed(
+                            hintText: 'Your email Address',
+                            hintStyle: subTitleTextStyle,
+                          ))),
+                ],
+              )),
+            )
+          ],
+        ),
+      );
+    }
+
+    Widget passwordInput() {
+      return Container(
+        margin: EdgeInsets.only(top: 20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Password',
+              style:
+                  primaryTextStyle.copyWith(fontSize: 16, fontWeight: medium),
+            ),
+            SizedBox(
+              height: 12,
+            ),
+            Container(
+              height: 50,
+              padding: EdgeInsets.symmetric(horizontal: 16),
+              decoration: BoxDecoration(
+                  color: backgroundColor2,
+                  borderRadius: BorderRadius.circular(12)),
+              child: Center(
+                  child: Row(
+                children: [
+                  Image.asset(
+                    'assets/icon_password.png',
+                    width: 17,
+                  ),
+                  SizedBox(
+                    width: 16,
+                  ),
+                  Expanded(
+                      child: TextFormField(
+                          obscureText: true,
+                          style: primaryTextStyle,
+                          decoration: InputDecoration.collapsed(
+                            hintText: 'Your Password',
+                            hintStyle: subTitleTextStyle,
+                          ))),
+                ],
+              )),
+            )
+          ],
+        ),
+      );
+    }
+
+    Widget signUpButton() {
+      return Container(
+        height: 50,
+        width: double.infinity,
+        margin: EdgeInsets.only(top: 30),
+        child: TextButton(
+          onPressed: () {
+            Navigator.pushNamed(context, '/home'
+            );},
+          style: TextButton.styleFrom(
+              backgroundColor: primaryColor,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(12))),
+          child: Text(
+            'Sign Up',
+            style: primaryTextStyle.copyWith(fontSize: 16, fontWeight: medium),
+          ),
+        ),
+      );
+    }
+
+    Widget footer() {
+      return Container(
+        margin: EdgeInsets.only(bottom: 30),
+        child: Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+          Text(
+            'Already have an account? ',
+            style: subTitleTextStyle,
+          ),
+          GestureDetector(
+            onTap: () {
+              Navigator.pushNamed(context, '/sign-in');
+            },
+            child: Text(
+              'Sign In',
+              style: purpleTextStyle.copyWith(fontSize: 12, fontWeight: medium),
+            ),
+          )
+        ]),
+      );
+    }
+
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      backgroundColor: backgroundColor1,
+      body: Container(
+        margin: EdgeInsets.symmetric(
+          horizontal: defaultMargin,
+        ),
+        child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+          header(),
+          fullNameInput(),
+          usernameInput(),
+          emailInput(),
+          passwordInput(),
+          signUpButton(),
+          Spacer(),
+          footer()
+        ]),
+      ),
+    );
+  }
+}
---
+++ lib/theme.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+double defaultMargin = 30.0;
+
+Color primaryColor = Color(0xff6C5ECF);
+Color secondaryColor = Color(0xff38ABBE);
+Color alertColor = Color(0xffED6363);
+Color priceColor = Color(0xff2C96F1);
+
+//Background Color
+Color backgroundColor1 = Color(0xff1F1D2B);
+Color backgroundColor2 = Color(0xff2B2937);
+Color backgroundColor3 = Color(0xff242231);
+Color backgroundColor4 = Color(0xff252836);
+Color backgroundColor5 = Color(0xff2B2844);
+
+//Text Color
+Color primaryTextColor = Color(0xffF1F0F2);
+Color secondaryTextColor = Color(0xff999999);
+Color subTitleColor = Color(0xFF504F5E);
+Color transparentColor = Colors.transparent;
+Color blackColor = Color(0xff2E2E2E);
+
+//TextStyle
+TextStyle primaryTextStyle = GoogleFonts.poppins(
+  color: primaryTextColor,
+);
+TextStyle secondaryTextStyle = GoogleFonts.poppins(
+  color: secondaryTextColor,
+);
+TextStyle priceTextStyle = GoogleFonts.poppins(
+  color: priceColor,
+);
+TextStyle subTitleTextStyle = GoogleFonts.poppins(
+  color: subTitleColor,
+);
+TextStyle purpleTextStyle = GoogleFonts.poppins(
+  color: primaryColor,
+);
+TextStyle blackTextStyle = GoogleFonts.poppins(
+  color: blackColor,
+);
+
+FontWeight light = FontWeight.w300;
+FontWeight reguler = FontWeight.w400;
+FontWeight medium = FontWeight.w500;
+FontWeight semiBold = FontWeight.w600;
+FontWeight bold = FontWeight.w700;
